d3905fcb013bcb1cd7968c40ef57f6b42627fd1d
Denis Avdeev <davdeev@me.com>
2025-12-25 11:40:06 +0500

    Initial Commit

---
+++ CodeWord Breaker/CodeWord_BreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeWord_BreakerApp.swift
+//  CodeWord Breaker
+//
+//  Created by Denis Avdeev on 25.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CodeWord_BreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
